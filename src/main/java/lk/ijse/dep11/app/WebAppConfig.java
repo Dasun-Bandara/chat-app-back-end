20dbf71894aefae59717389314cdf62bc0a2f8a7
Dasun Bandara <dasun8517@gmail.com>
2024-02-20 00:27:22 +0530

    Setup Database

--- src/main/java/lk/ijse/dep11/app/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/app/WebAppConfig.java
@@ -1,11 +1,30 @@
 package lk.ijse.dep11.app;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.sql.SQLException;
+
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
+@PropertySource("/application.properties")
 public class WebAppConfig {
+    @Bean(destroyMethod = "close")
+    public HikariDataSource pool(Environment env) throws SQLException {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(env.getRequiredProperty("app.datasource.drive_class_name"));
+        config.setJdbcUrl(env.getRequiredProperty("app.datasource.url"));
+        config.setUsername(env.getRequiredProperty("app.datasource.username"));
+        config.setPassword(env.getRequiredProperty("app.datasource.password"));
+        config.setMaximumPoolSize(env.getRequiredProperty("app.datasource.max_pool_size", Integer.class));
+        return new HikariDataSource(config);
+    }
 }
